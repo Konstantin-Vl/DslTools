e74d68302b697f492bab93b65c10d65ce5374ca9
Konstantin Voloshchuk <kosv.dev@gmail.com>
2018-01-06 23:20:20 +0600

    Added auto convert to utf-8

--- src/FileObject.php
+++ src/FileObject.php
@@ -3,16 +3,24 @@
 namespace Kosv\DslTools;
 
 use SplFileObject as BaseFileObject;
+use Kosv\DslTools\Helpers\FileHelper;
 
 class FileObject extends BaseFileObject
 {
+
+    const REQUIRED_ENCODING = 'UTF-16';
+
     /**
      * FileObject constructor.
+     *
      * @param string $fileName
      * @param string $openMode
      */
     public function __construct($fileName, $openMode = 'r')
     {
-        // TODO: Implement FileObject class.
+        parent::__construct(
+            FileHelper::convertToUtf8($fileName, self::REQUIRED_ENCODING),
+            $openMode
+        );
     }
 }
