567c626ccd692f098061c7eedc14819c5d46b4bb
Konstantin Voloshchuk <kosv.dev@gmail.com>
2018-01-07 22:25:30 +0600

    Implemented class CardsIterator

--- src/Iterators/CardsIterator.php
+++ src/Iterators/CardsIterator.php
@@ -3,11 +3,29 @@
 namespace Kosv\DslTools\Iterators;
 
 use Countable;
+use OutOfRangeException;
 use Kosv\DslTools\Contracts\IReadable;
+use Kosv\DslTools\Card;
 use Kosv\DslTools\Document;
+use Kosv\DslTools\Parser;
 
 class CardsIterator implements IReadable, Countable
 {
+    /**
+     * @var array
+     */
+    private $cardPointers = [];
+
+    /**
+     * @var int
+     */
+    private $currentCard = 0;
+
+    /**
+     * @var \Kosv\DslTools\Document
+     */
+    private $document;
+
     /**
      * CardsIterator constructor.
      *
@@ -15,23 +33,34 @@ class CardsIterator implements IReadable, Countable
      */
     public function __construct(Document $document)
     {
-        // TODO: Implement __construct() method.
+        $this->document = $document;
+        $this->parseCards();
     }
 
     /**
-     * @return \Kosv\DslTools\Card|null
+     * @return \Kosv\DslTools\Card
      */
     public function current()
     {
-        // TODO: Implement current() method.
+        $stream = $this->document->getStream();
+
+        $begin = $this->getPointers($this->key())['begin'];
+        $end   = $this->getPointers($this->key())['end'];
+        $cardData = [];
+        for ($i = $begin; $i <= $end; $i++) {
+            $stream->seek($i);
+            $cardData[] = $stream->current();
+        }
+
+        return new Card($cardData);
     }
 
     /**
-     * @return \Kosv\DslTools\Card|null
+     * @return void
      */
     public function next()
     {
-        // TODO: Implement next() method.
+        $this->seek($this->key() + 1);
     }
 
     /**
@@ -39,7 +68,7 @@ class CardsIterator implements IReadable, Countable
      */
     public function key()
     {
-        // TODO: Implement key() method.
+        return $this->currentCard;
     }
 
     /**
@@ -47,7 +76,11 @@ class CardsIterator implements IReadable, Countable
      */
     public function valid()
     {
-        // TODO: Implement valid() method.
+        if ($this->key() + 1 > $this->count() - 1) {
+            return false;
+        }
+
+        return true;
     }
 
     /**
@@ -55,7 +88,7 @@ class CardsIterator implements IReadable, Countable
      */
     public function rewind()
     {
-        // TODO: Implement rewind() method.
+        $this->seek(0);
     }
 
     /**
@@ -63,7 +96,7 @@ class CardsIterator implements IReadable, Countable
      */
     public function count()
     {
-        // TODO: Implement count() method.
+        return count($this->cardPointers);
     }
 
     /**
@@ -72,6 +105,56 @@ class CardsIterator implements IReadable, Countable
      */
     public function seek($cardIndex)
     {
-        // TODO: Implement seek() method.
+        if ($cardIndex > $this->count() - 1 || $cardIndex < 0) {
+            throw new OutOfRangeException("Invalid card index: {$cardIndex}");
+        }
+
+        $this->currentCard = $cardIndex;
+    }
+
+    /**
+     * @param int $cardIndex
+     * @return array
+     */
+    protected function getPointers($cardIndex)
+    {
+        if (!isset($this->cardPointers[$cardIndex])) {
+            throw new OutOfRangeException("No data for card: {$cardIndex}");
+        }
+
+        return $this->cardPointers[$cardIndex];
+    }
+
+    /**
+     * @return void
+     */
+    protected function parseCards()
+    {
+        $stream = $this->document->getStream();
+        $stream->rewind();
+
+        $parser = new Parser();
+        $beginCard = null; $endCard = null;
+
+        while ($stream->valid()) {
+            if ($parser->match(Parser::CARD_HEAD, $stream->current())) {
+                $beginCard = $stream->key();
+                if ($stream->valid()) {
+                    $stream->next();
+                    while ($stream->valid() && $parser->match(Parser::CARD_BODY, $stream->current())) {
+                        $endCard = $stream->key();
+                        $stream->next();
+                    }
+                }
+
+            }
+            if (is_int($beginCard) && is_int($endCard)) {
+                $this->cardPointers[] = ['begin' => $beginCard, 'end' => $endCard];
+                $beginCard = null; $endCard = null;
+                continue;
+            }
+
+            $stream->next();
+        }
     }
 }
